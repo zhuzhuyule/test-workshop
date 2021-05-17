5e7b2dfe118c4a067827b310a62253d3a7f2d627
Pengfei Zhu <zhuzhuyule@126.com>
2021-05-17 14:21:31 +0800

    补充异步测试用例

--- src/app/services/math.service.spec.ts
+++ src/app/services/math.service.spec.ts
@@ -10,16 +10,21 @@ describe("MathService", () => {
     expect(mathService.add(1, 2, 3)).toBe(6);
   });
 
-  it("#square should return 4 from a promise when the arg is 2", async () => {
+  it("#square should return 4 from a promise when the arg is 2 (use await)", async () => {
     const value = await mathService.square(2);
     expect(value).toBe(4);
   });
 
-  // 另外一种做法
-  it("#square should return 4 from a promise when the arg is 2", (done: DoneFn) => {
+  it("#square should return 4 from a promise when the arg is 2 (use done function)", (done: DoneFn) => {
     mathService.square(2).then((value) => {
       expect(value).toBe(4);
       done();
+    });
+  });
+
+  it("#square should return 4 from a promise when the arg is 2 (use return value)", () => {
+    return mathService.square(2).then((value) => {
+      return expect(value).toBe(4);
     });
   });
 });
