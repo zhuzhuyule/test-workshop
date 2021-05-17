be4ea69a88fcb01580ccac40276d312296e0ee65
Pengfei Zhu <zhuzhuyule@126.com>
2021-05-17 15:59:32 +0800

    添加基础测试

--- src/app/components/terminal/terminal.component.spec.ts
+++ src/app/components/terminal/terminal.component.spec.ts
@@ -1,4 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { TerminalComponent } from './terminal.component';
 
@@ -8,7 +9,11 @@ describe('TerminalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TerminalComponent ]
+      imports: [ FormsModule ],
+      declarations: [ TerminalComponent ],
+      providers: [
+        // { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +27,25 @@ describe('TerminalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('abouts extend input', () => {
+    beforeEach(() => {
+      component.canChange = true;
+    });
+
+    it('should show extend input when the "canChange" is true', () => {
+      fixture.detectChanges()
+      expect(fixture.nativeElement.querySelector('.input-command')).toBeTruthy();
+    });
+  
+    it('should show one customer input when the "canChange" is true and has two command', () => {
+      component.commands = ['ng g c', 'ng g component'];
+      fixture.detectChanges()
+  
+      const commandInputs = fixture.nativeElement.querySelectorAll('.input-command');
+      expect(commandInputs.length).toBe(1);
+    });
+  });
+
+  
 });
